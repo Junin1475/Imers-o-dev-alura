e90321d413d39203650a070add86adebc8d0be25
Julio Resende <jjiir1475@gmail.com>
2021-03-23 18:16:06 -0300

    adicionando pastas

---
+++ Aula 01/JS/func.js
@@ -0,0 +1,16 @@
+var valorEmDolarTexto = prompt("Qual valor em dólar você quer converter?")
+
+var valorEmDolarTexto = parseFloat(valorEmDolarTexto)
+
+var valorEmReal = valorEmDolarTexto* 5.54
+var valorEmRealFixado = valorEmReal.toFixed (2)
+
+alert("O Valor em Real é R$ " + valorEmRealFixado)
+
+//Revisão
+
+//variáveis var int - float - string
+
+//alert - parseInt - parseFloat - prompt 
+
+//operações + somar * multiplicar 
