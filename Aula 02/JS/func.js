fff52ecb5ec7eb5b35cd2eee0b935e5940a80fbc
Julio Resende <jjiir1475@gmail.com>
2021-03-23 22:41:05 -0300

    alteração aula 2

---
+++ Aula 02/JS/func.js
@@ -0,0 +1,21 @@
+var primeiroValor = parseInt(prompt("Digite o primeiro valor:"))
+var segundoValor = parseInt(prompt("Digite o segundo valor:"))
+var resultado = primeiroValor / segundoValor
+
+var operacao = prompt("Digite / caso queira dividir, * para multiplicar, + para somar e - para subtrair: ")
+
+if (operacao == 1) {
+ var resultado = primeiroValor / segundoValor
+ document.write("<h2>" + primeiroValor + " / " + segundoValor + " = " + resultado + "</h2>")
+} else if (operacao == 2){
+ var resultado = primeiroValor * segundoValor
+ document.write("<h2>" + primeiroValor + " x " + segundoValor + " = " + resultado + "</h2>")
+} else if (operacao == 3){
+   var resultado = primeiroValor + segundoValor
+ document.write("<h2>" + primeiroValor + " + " + segundoValor + " = " + resultado + "</h2>")
+} else if(operacao == 4){
+   var resultado = primeiroValor - segundoValor
+ document.write("<h2>" + primeiroValor + " - " + segundoValor + " = " + resultado + "</h2>")
+} else {
+  document.write("<h2>Opção inválida</h2>")
+}
