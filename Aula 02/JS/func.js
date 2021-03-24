4334ece8bca300a55802834d726bd6cb9998b289
Julio Resende <jjiir1475@gmail.com>
2021-03-24 18:03:33 -0300

    Aula 03

--- Aula 02/JS/func.js
+++ Aula 02/JS/func.js
@@ -2,18 +2,18 @@ var primeiroValor = parseInt(prompt("Digite o primeiro valor:"))
 var segundoValor = parseInt(prompt("Digite o segundo valor:"))
 var resultado = primeiroValor / segundoValor
 
-var operacao = prompt("Digite / caso queira dividir, * para multiplicar, + para somar e - para subtrair: ")
+var operacao = prompt("Digite 1 caso queira dividir, 2 para multiplicar, 3 para somar e 4 para subtrair: ")
 
-if (operacao == "/") {
+if (operacao == 1) {
  var resultado = primeiroValor / segundoValor
  document.write("<h2>" + primeiroValor + " / " + segundoValor + " = " + resultado + "</h2>")
-} else if (operacao == "*"){
+} else if (operacao == 2 ){
  var resultado = primeiroValor * segundoValor
  document.write("<h2>" + primeiroValor + " x " + segundoValor + " = " + resultado + "</h2>")
-} else if (operacao == "+"){
+} else if (operacao ==  3 ){
    var resultado = primeiroValor + segundoValor
  document.write("<h2>" + primeiroValor + " + " + segundoValor + " = " + resultado + "</h2>")
-} else if(operacao == "-"){
+} else if(operacao == 4 ){
    var resultado = primeiroValor - segundoValor
  document.write("<h2>" + primeiroValor + " - " + segundoValor + " = " + resultado + "</h2>")
 } else {
